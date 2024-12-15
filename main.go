4b04dd8b97d8680eac39e4eae80645a9443385d5
paulina <paulina.nc.1419@gmail.com>
2024-12-14 21:56:21 -0500

    Read from file

--- main.go
+++ main.go
@@ -3,18 +3,22 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"log"
 	"os"
 )
 
 func main() {
-	fmt.Println("What is your name? >")
-	reader := bufio.NewReader(os.Stdin)
-	line, err := reader.ReadString('\n')
+	// fmt.Println("What is your name? >")
+	file, _ := os.OpenFile("file.txt", os.O_RDONLY, 0666)
+	defer file.Close()
+	reader := bufio.NewReader(file)
+	for {
 
-	if err != nil {
-		log.Fatal(err)
+		readFile, err := reader.ReadString('\n')
+		fmt.Printf("-> %s", readFile)
+		if err != nil {
+			return
 
+		}
 	}
-	fmt.Printf("Hello %s\n", line)
+
 }
